66e6e7cd6b00f70d911d31622e8831346e81e77d
Yanni Papandreou <nepo1y@gmail.com>
2026-01-16 17:51:00 +0000

    handle invalid commands

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,15 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
+var stdin_buffer: [4096]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+const stdin = &stdin_reader.interface;
+
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
+    // Print the prompt
     try stdout.print("$ ", .{});
+
+    // Capture the user's command
+    const command = try stdin.takeDelimiter('\n');
+    try stdout.print("{s}: command not found\n", .{command.?});
 }
