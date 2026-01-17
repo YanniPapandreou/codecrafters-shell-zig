2c1e2eb9551eed18271014d114c61a9557892e4f
Yanni Papandreou <nepo1y@gmail.com>
2026-01-17 22:37:06 +0000

    implemented first version of history

--- src/builtins.zig
+++ src/builtins.zig
@@ -5,13 +5,44 @@ const utils = @import("utils");
 const ParserError = utils.ParserError;
 const RuntimeError = utils.RuntimeError;
 
+pub const History = struct {
+    allocator: mem.Allocator,
+    history: std.ArrayList([]const u8),
+
+    pub fn init(allocator: mem.Allocator) !History {
+        const hist = try std.ArrayList([]const u8).initCapacity(allocator, 0);
+        return History{
+            .allocator = allocator,
+            .history = hist,
+        };
+    }
+
+    pub fn deinit(self: *History) void {
+        for (self.history.items) |line| {
+            self.allocator.free(line);
+        }
+        self.history.deinit(self.allocator);
+    }
+
+    pub fn append(self: *History, input: []const u8) !void {
+        const input_copy = try self.allocator.alloc(u8, input.len);
+        @memcpy(input_copy, input);
+        try self.history.append(self.allocator, input_copy);
+    }
+
+    fn print(self: *History, writer: *std.io.Writer) !void {
+        for (self.history.items, 1..) |entry, i| {
+            try writer.print("   {d}  {s}\n", .{i, entry});
+        }
+    }
+};
+
 pub fn echo(writer: *std.io.Writer, args: []const u8) !void {
     try writer.print("{s}\n", .{args});
 }
 
-pub fn history(writer: *std.io.Writer, args: []const u8) !void {
-    _ = args;
-    try writer.print("TODO: implement history builtin\n", .{});
+pub fn history(writer: *std.io.Writer, hist: *History) !void {
+    try hist.print(writer);
 }
 
 pub fn type_of_cmd(allocator: mem.Allocator, writer: *std.io.Writer, args: []const u8) !void {
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const builtin = @import("builtin");
 const mem = std.mem;
 const builtins = @import("builtins");
 const utils = @import("utils");
+const History = builtins.History;
 
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
@@ -15,6 +16,8 @@ var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const repl_allocator = gpa.allocator();
 
 pub fn main() !void {
+    var repl_history = try History.init(repl_allocator);
+    defer repl_history.deinit();
     while (true) {
         // Print the prompt
         try stdout.print("$ ", .{});
@@ -23,14 +26,14 @@ pub fn main() !void {
         const command = try stdin.takeDelimiter('\n');
 
         if (command) |cmd| {
+            try repl_history.append(cmd);
             if (mem.eql(u8, cmd, "exit")) {
                 break;
             } else if (mem.startsWith(u8, cmd, "echo")) {
                 const args = try utils.get_args("echo", cmd);
                 try builtins.echo(stdout, args);
-            } else if (mem.startsWith(u8, cmd, "history")) {
-                const args = try utils.get_args("history", cmd);
-                try builtins.history(stdout, args);
+            } else if (mem.eql(u8, cmd, "history")) {
+                try builtins.history(stdout, &repl_history);
             } else if (mem.startsWith(u8, cmd, "type")) {
                 const args = try utils.get_args("type", cmd);
                 try builtins.type_of_cmd(repl_allocator, stdout, args);
